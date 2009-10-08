01af86028034a05171ca8d769ea352bbf73cce65
Mwjuhl@gmail.com <Mwjuhl@gmail.com@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-08 08:28:54 +0000

    Database connector

---
+++ src/dbConnect/Constant.java
@@ -0,0 +1,16 @@
+package dbConnect;
+
+
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+	server					= "grp3.smallmeans.com",  // database-serveren
+	database				= "grp3", // navnet på databasen
+	username				= "s073197", // brugernavnet 
+	password				= "hej312"; // password
+	
+	public static final int
+		port					= 3306;
+}
