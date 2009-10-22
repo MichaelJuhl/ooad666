574eaf2c7d5e76131ebd93b8dd9edd047b4b9683
Mwjuhl@gmail.com <Mwjuhl@gmail.com@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-22 07:04:12 +0000

    Update af IEvent

--- trunk/src/dalinterface/IEvent.java
+++ trunk/src/dalinterface/IEvent.java
@@ -3,13 +3,20 @@ package dalinterface;
 import java.sql.SQLException;
 import java.util.List;
 
-import dao.EventDAO;
+import dal.Event;
 
 public interface IEvent {
 
-	EventDAO getEventID() throws DALException;
+	Event getEvent(int EventID) throws DALException;
+	List<Event> getEventList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
+	void createEvent(Event EventID) throws DALException;
+	void updateEvent(int EventID, Event Event) throws DALException;
+	void sletEvent(int EventID) throws DALException;
+	public boolean checkEvent(String datestart, String stage) throws DALException;
 	
 }
+	
+
 
 
 
