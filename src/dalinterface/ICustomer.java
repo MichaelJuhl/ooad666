de5d5fbef7e3b47092c2c8048de0c72779c4bf42
AbdiL337@gmail.com <AbdiL337@gmail.com@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-22 09:00:58 +0000

    Færdig med Event

--- src/dalinterface/ICustomer.java
+++ src/dalinterface/ICustomer.java
@@ -1,5 +1,18 @@
 package dalinterface;
 
-public class ICustomer {
+import java.sql.SQLException;
+import java.util.List;
+
+import dal.Customer;
+import dao.CustomerDAO;
+
+public interface ICustomer {
+	
+	Customer getCustomer(int CustomerID) throws DALException;
+	List<Customer> getCustomerList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
+	void createCustomer(Customer CustomerID ,Customer Customer) throws DALException;
+	void updateCustomers(int CustomerID, Customer Customers) throws DALException;
+	void sletCustomer(int Customer) throws DALException;
+
 
 }
--- src/dalinterface/IEvent.java
+++ src/dalinterface/IEvent.java
@@ -9,11 +9,11 @@ public interface IEvent {
 
 	Event getEvent(int EventID) throws DALException;
 	List<Event> getEventList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
-	void createEvent(Event EventID) throws DALException;
+	void createEvent(Event EventID, Event Event) throws DALException;
 	void updateEvent(int EventID, Event Event) throws DALException;
 	void sletEvent(int EventID) throws DALException;
-	public boolean checkEvent(String datestart, String stage) throws DALException;
-	
+	public boolean checkEvent(Event Event) throws DALException;
+	public boolean CheckTickets(Event Event) throws DALException;
 }
 	
 
