9a73b8c153a23f72ea476fea4d400b3391d92c6b
svantetobias <svantetobias@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-22 09:57:19 +0000

    Event update og test af DateFormat.

--- trunk/src/test/TestDateFormat.java
+++ trunk/src/test/TestDateFormat.java
@@ -4,7 +4,7 @@ import dal.Event;
 
 public class TestDateFormat {
 	public TestDateFormat() throws Exception {
-		Event event = new Event(3,"Rock","Gul","Bamse","2010-09-22 12:00:00","2010-09-22 12:00:00","Bamse spiller",154,200);
+		Event event = new Event(3,"Rock","Gul","Bamse","2010-09-22 12:49:00","2010-09-22 12:00:00","Bamse spiller",154,200);
 		System.out.println(event.getDateStart());
 		System.out.println(event.getDateFinish());
 	}
