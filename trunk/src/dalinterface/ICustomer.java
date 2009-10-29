14a8e88839672a43a846716d5086b72690eb728d
Mwjuhl@gmail.com <Mwjuhl@gmail.com@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-29 09:39:43 +0000

    Ændret Interface >List til ArrayList

--- trunk/src/dalinterface/ICustomer.java
+++ trunk/src/dalinterface/ICustomer.java
@@ -1,15 +1,15 @@
 package dalinterface;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 import dal.Customer;
-import dao.CustomerDAO;
+
 
 public interface ICustomer {
 	
 	Customer getCustomer(int CustomerID) throws DALException;
-	List<Customer> getCustomerList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
+	ArrayList<Customer> getCustomerList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
 	void createCustomer(Customer CustomerID ,Customer Customer) throws DALException;
 	void updateCustomers(int CustomerID, Customer Customers) throws DALException;
 	void sletCustomer(int Customer) throws DALException;
--- trunk/src/dalinterface/IEvent.java
+++ trunk/src/dalinterface/IEvent.java
@@ -2,14 +2,14 @@ package dalinterface;
 
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.util.List;
+import java.util.ArrayList;
 
 import dal.Event;
 
 public interface IEvent {
 
 	Event getEvent(int EventID) throws DALException, ParseException;
-	List<Event> getEventList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, ParseException;
+	ArrayList<Event> getEventList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, ParseException;
 	void createEvent(Event EventID, Event Event) throws DALException;
 	void updateEvent(int EventID, Event Event) throws DALException;
 	void sletEvent(int EventID) throws DALException;
