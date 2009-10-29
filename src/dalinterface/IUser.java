ff516b0f08aa49f51e3c00a58f29cff2a9883c84
Mwjuhl@gmail.com <Mwjuhl@gmail.com@6d67d298-ae6d-11de-bed4-89e45268dd3d>
2009-10-29 09:39:43 +0000

    Ændret Interface >List til ArrayList

--- src/dalinterface/IUser.java
+++ src/dalinterface/IUser.java
@@ -1,14 +1,14 @@
 package dalinterface;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 import dal.User;
 
 public interface IUser {
 	
 	User getUser(int UserID) throws DALException;
-	List<User> getUserList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
+	ArrayList<User> getUserList() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException;
 	void createUser(User UserID,User Users) throws DALException;
 	void updateUser(int UserID, User Users) throws DALException;
 	void sletUser(int UserID) throws DALException;
